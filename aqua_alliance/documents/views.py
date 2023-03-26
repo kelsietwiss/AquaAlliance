261df6209bbd85fece60e1ded472bc750ca59c1e
kelsietwiss <118209239+kelsietwiss@users.noreply.github.com>
2023-03-26 15:15:45 -0700

    finishing touches

--- aqua_alliance/documents/views.py
+++ aqua_alliance/documents/views.py
@@ -5,6 +5,6 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def documents(request):
-    list = DocModel.objects.all().order_by('-published_date')
+    list = DocModel.objects.all().order_by('title')
     return render(request, "documents.html", {"list":list})
 
